484aeb3495c00a9e1f507102c7306555c21d4313
nizami <nizami.dev@gmail.com>
2025-07-08 17:09:12 +0300

    Remove $type symbol for object serialization

--- src/is.ts
+++ src/is.ts
@@ -1,5 +1,5 @@
-import {$Type, $type} from '#lib';
+import {$Type} from '#lib';
 
 export function is<T extends $Type>(model: any, type: T): model is Exclude<T['type'], undefined> {
-  return (model?.[$type] as $Type)?.is(type) ?? false;
+  return (model?.$type as $Type)?.is(type) ?? false;
 }
--- src/model.ts
+++ src/model.ts
@@ -1,9 +1,7 @@
 import {$NewType, $Type} from '#lib';
 
-export const $type = Symbol('type');
-
 export interface Model {
-  [$type]: $Type;
+  $type: $Type;
 }
 
 export const $Model = () => $NewType<Model>('RuntimeType', 'Model');
